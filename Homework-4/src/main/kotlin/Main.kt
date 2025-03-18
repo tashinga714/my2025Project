c8ce68449c5b9e85a8c8b9d8b4e3462d1b0c68c0
muchsimon <smuchinenyika@nust.na>
2025-03-18 15:22:20 +0200

    Delete Homework-4 directory

--- Homework-4/src/main/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-import org.jub.kotlin.hometask4.Application
-import org.jub.kotlin.hometask4.tasks
-
-fun main() {
-    val app = Application.create("results.txt", tasks)
-    app.run()
-    app.waitToFinish()
-}
